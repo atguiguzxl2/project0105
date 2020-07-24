2d2d84659c727eda85aac02932d7ca5bd42d969f
zxl <1533@qq.com>
2020-07-24 15:57:54 +0800

    第三次提交

--- demo2/src/main/java/com/example/demo/Test.java
+++ demo2/src/main/java/com/example/demo/Test.java
@@ -13,4 +13,10 @@ public class Test {
     public String get(){
         return "hello";
     }
+
+    @RequestMapping("get1")
+    @ResponseBody
+    public String get1(){
+        return "hello1";
+    }
 }
